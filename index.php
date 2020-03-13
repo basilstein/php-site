123ac5917ea8ad4282c73a51cb6afc25b235ef84
basilstein <44881303+basilstein@users.noreply.github.com>
2020-03-13 12:19:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 </head>
 <body>
 	<?php 
-	echo "<img src = 'image.jpg'>"
-	//$var=php:input"check";
+	echo "<img src = 'image.jpg'><br>";
+	$var=array(3,4,5);
+	print_r($var);
+	echo "<br>";
+	var_dump($var);
+	$city = $_POST['city'];
+	echo "<br>";
+	var_dump($city);
 	?>
 </body>
 </html>
